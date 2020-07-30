feb7132cba5227a7fd12d02f16232003cb2a2d27
Valerii Ryndin <ryndin20@yandex.ru>
2020-07-30 07:27:07 +0300

    models fixes

--- app/mfos/models.py
+++ app/mfos/models.py
@@ -13,23 +13,26 @@ class Lender(models.Model):
     # logo = models.ImageField()  # TODO upload_to='images/', add pillow
 
     is_legal = models.BooleanField(default=False)
-    type = models.TextField(blank=True)
-    regdate = models.DateTimeField('date entry added to cbr register', blank=True, null=True)
-    regnum = models.IntegerField('number in cbr register', unique=True, null=True)
-    ogrn = models.IntegerField(unique=True, null=True)
-    inn = models.IntegerField(unique=True, null=True)
+    type = models.TextField('Вид МФО', blank=True)
+    regdate = models.DateTimeField('Дата внесения в ЦБ', blank=True, null=True)
+    regnum = models.IntegerField('Регномер ЦБ', unique=True, null=True)
+    ogrn = models.IntegerField('ОГРН', unique=True, null=True)
+    inn = models.IntegerField('ИНН', unique=True, null=True)
 
-    website = models.URLField(blank=True)  # both blank and null?
+    website = models.URLField(blank=True)
     email = models.EmailField(blank=True)
     socials = ArrayField(models.URLField(), null=True)
     address = models.TextField('Адрес', blank=True)
     head_name = models.TextField('Руководитель', blank=True)
 
-    amount_min = models.IntegerField()
-    amount_max = models.IntegerField()
-    overpayment_day = models.IntegerField()
-    overpayment_full = models.IntegerField()
-    refusal_reasons = ArrayField(models.TextField())
+    amount_min = models.IntegerField('Минимальная сумма займа')
+    amount_max = models.IntegerField('Максимальная сумма займа')
+    overpayment_day = models.IntegerField('Переплата за день')
+    overpayment_full = models.IntegerField('Переплата за весь срок')
+    refusal_reasons = ArrayField(models.TextField('Причины отказа'))
+
+    def __str__(self):
+        return self.trademark or self.name
 
 
 class Document(models.Model):
@@ -48,12 +51,12 @@ class Loan(models.Model):
     last_modified = models.DateTimeField('Дата актуализации')
     # Условия и ставки
     purpose = ArrayField(models.TextField('Цель займа'))
-    amount_max = models.TextField('Максимальная сумма займа')
-    amount_note = models.TextField('Допинфа по сумме займа')  # first_loan_condition
-    interest_rate = models.TextField('Ставка')  # rate
+    amount_max = models.IntegerField('Максимальная сумма займа')
+    amount_note = models.TextField('Допинфа по сумме займа', blank=True),  # first_loan_condition
+    rate = models.TextField('Ставка')
     period_min = models.IntegerField('Минимальный срок займа')  # dates_from
     period_max = models.IntegerField('Максимальный срок займа')  # dates_to
-    period_note = models.TextField('Допинфа по срокам')  # loan_time_terms
+    period_note = models.TextField('Допинфа по срокам', blank=True)  # loan_time_terms
     collateral = ArrayField(models.TextField('Обеспечение'))  # providing
     # Требования и документы
     borrowers_categories = ArrayField(models.TextField('Категория заемщиков'))
@@ -64,10 +67,10 @@ class Loan(models.Model):
     issuance = models.TextField('Срок выдачи')
     loan_processing = ArrayField(models.TextField('Оформление займа'))
     loan_form = ArrayField(models.TextField('Форма выдачи'))
-    loan_form_note = models.TextField('Допинфа по форме выдачи')  # loan_form_description
+    loan_form_note = models.TextField('Допинфа по форме выдачи', blank=True)  # loan_form_description
     # Погашение
     repayment_order = ArrayField(models.TextField('Порядок погашения'))
-    repayment_order_note = models.TextField('Допинфа по порядку погашения')  # repayment_order_description
+    repayment_order_note = models.TextField('Допинфа по порядку погашения', blank=True)  # repayment_order_description
     payment_methods = ArrayField(models.TextField('Способ оплаты'))
 
     # Об организации
@@ -80,3 +83,6 @@ class Loan(models.Model):
     head_name = models.TextField('Руководитель')
     regnum = models.IntegerField()
     ogrn = models.IntegerField()
+
+    def __str__(self):
+        return self.name
