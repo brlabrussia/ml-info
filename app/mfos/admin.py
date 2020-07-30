815ed808a98646cc7a6234b114522fafd86a1d7e
Valerii Ryndin <ryndin20@yandex.ru>
2020-07-30 07:09:27 +0300

    lender and loan model

--- app/mfos/admin.py
+++ app/mfos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mfos.models import MFO, Loan, Test
+from mfos.models import Lender, Loan
 
-admin.site.register(MFO)
+admin.site.register(Lender)
 admin.site.register(Loan)
--- app/mfos/models.py
+++ app/mfos/models.py
@@ -2,12 +2,12 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-class MFO(models.Model):
+class Lender(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     scraped_from = ArrayField(models.URLField())
 
-    trademark = models.TextField(blank=True)
+    trademark = models.TextField('Торговая марка', blank=True)
     name = models.TextField(blank=True)
     full_name = models.TextField(blank=True)
     # logo = models.ImageField()  # TODO upload_to='images/', add pillow
@@ -22,49 +22,61 @@ class MFO(models.Model):
     website = models.URLField(blank=True)  # both blank and null?
     email = models.EmailField(blank=True)
     socials = ArrayField(models.URLField(), null=True)
-    address = models.TextField(blank=True)
-    head_name = models.TextField(blank=True)
+    address = models.TextField('Адрес', blank=True)
+    head_name = models.TextField('Руководитель', blank=True)
 
-    money_value_min = models.IntegerField()
-    money_value_max = models.IntegerField()
+    amount_min = models.IntegerField()
+    amount_max = models.IntegerField()
     overpayment_day = models.IntegerField()
     overpayment_full = models.IntegerField()
     refusal_reasons = ArrayField(models.TextField())
 
 
 class Document(models.Model):
-    mfo = models.ForeignKey(MFO, on_delete=models.CASCADE)
+    lender = models.ForeignKey(Lender, on_delete=models.CASCADE)
     name = models.TextField()
     file = models.FileField()
 
 
-# WIP
 class Loan(models.Model):
-    address = models.TextField()
-    borrowers_age = models.TextField()
-    borrowers_categories = ArrayField(models.TextField())
-    borrowers_documents = ArrayField(models.TextField())
-    borrowers_registration = ArrayField(models.TextField())
-    dates_from = models.IntegerField()
-    dates_to = models.IntegerField()
-    first_loan_condition = models.TextField()
-    head_name = models.TextField()
-    issuance = models.TextField()
-    loan_form = ArrayField(models.TextField())
-    loan_form_description = models.TextField()
-    loan_processing = ArrayField(models.TextField())
-    loan_providing = ArrayField(models.TextField())
-    loan_purpose = ArrayField(models.TextField())
-    loan_time_terms = models.TextField()
-    # logo = models.ImageField()  # TODO upload_to arg
-    max_money_value = models.TextField()
-    mfo = models.ForeignKey(MFO, on_delete=models.CASCADE)
-    name = models.TextField()
-    ogrn = models.TextField()
-    payment_methods = ArrayField(models.TextField())
-    rate = models.TextField()
-    repayment_order = ArrayField(models.TextField())
-    repayment_order_description = models.TextField()
-    trademark = models.TextField()
-    last_modified = models.DateTimeField('last updated on source')
-    url = models.URLField('first scraped from', unique=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    scraped_from = models.URLField(unique=True)
+
+    # О займе
+    name = models.TextField('Название займа')
+    last_modified = models.DateTimeField('Дата актуализации')
+    # Условия и ставки
+    purpose = ArrayField(models.TextField('Цель займа'))
+    amount_max = models.TextField('Максимальная сумма займа')
+    amount_note = models.TextField('Допинфа по сумме займа')  # first_loan_condition
+    interest_rate = models.TextField('Ставка')  # rate
+    period_min = models.IntegerField('Минимальный срок займа')  # dates_from
+    period_max = models.IntegerField('Максимальный срок займа')  # dates_to
+    period_note = models.TextField('Допинфа по срокам')  # loan_time_terms
+    collateral = ArrayField(models.TextField('Обеспечение'))  # providing
+    # Требования и документы
+    borrowers_categories = ArrayField(models.TextField('Категория заемщиков'))
+    borrowers_age = models.TextField('Возраст заемщика')
+    borrowers_registration = ArrayField(models.TextField('Регистрация'))
+    borrowers_documents = ArrayField(models.TextField('Документы'))
+    # Выдача
+    issuance = models.TextField('Срок выдачи')
+    loan_processing = ArrayField(models.TextField('Оформление займа'))
+    loan_form = ArrayField(models.TextField('Форма выдачи'))
+    loan_form_note = models.TextField('Допинфа по форме выдачи')  # loan_form_description
+    # Погашение
+    repayment_order = ArrayField(models.TextField('Порядок погашения'))
+    repayment_order_note = models.TextField('Допинфа по порядку погашения')  # repayment_order_description
+    payment_methods = ArrayField(models.TextField('Способ оплаты'))
+
+    # Об организации
+    # Мерджим по этим данным каждый займ, но они могут быть устаревшими,
+    # поэтому для порядка сохраняем
+    lender = models.ForeignKey(Lender, on_delete=models.CASCADE)
+    logo = models.URLField()
+    trademark = models.TextField('Торговая марка')
+    address = models.TextField('Адрес')
+    head_name = models.TextField('Руководитель')
+    regnum = models.IntegerField()
+    ogrn = models.IntegerField()
