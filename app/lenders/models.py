d1522ec449e879e99fd1732fce97934e9eb140b0
Valerii Ryndin <ryndin20@yandex.ru>
2020-07-31 14:59:19 +0300

    models fixes - verbose names

--- app/lenders/models.py
+++ app/lenders/models.py
@@ -8,8 +8,8 @@ class Lender(models.Model):
     scraped_from = ArrayField(models.URLField())
 
     trademark = models.TextField('Торговая марка', blank=True)
-    name = models.TextField(blank=True)
-    full_name = models.TextField(blank=True)
+    name = models.TextField('Сокращенное наименование', blank=True)
+    full_name = models.TextField('Полное наименование', blank=True)
     # logo = models.ImageField()  # TODO upload_to='images/', add pillow
 
     is_legal = models.BooleanField(default=False)
@@ -19,9 +19,9 @@ class Lender(models.Model):
     ogrn = models.BigIntegerField('ОГРН', unique=True, blank=True, null=True)
     inn = models.BigIntegerField('ИНН', unique=True, blank=True, null=True)
 
-    website = models.URLField(blank=True)
-    email = models.EmailField(blank=True)
-    socials = ArrayField(models.URLField(), blank=True, null=True)
+    website = models.URLField('Вебсайт', blank=True)
+    email = models.EmailField('Электронная почта', blank=True)
+    socials = ArrayField(models.URLField(), verbose_name='Соцсети', blank=True, null=True)
     address = models.TextField('Адрес', blank=True)
     head_name = models.TextField('Руководитель', blank=True)
 
@@ -29,7 +29,7 @@ class Lender(models.Model):
     amount_max = models.IntegerField('Максимальная сумма займа', blank=True, null=True)
     overpayment_day = models.IntegerField('Переплата за день', blank=True, null=True)
     overpayment_full = models.IntegerField('Переплата за весь срок', blank=True, null=True)
-    refusal_reasons = ArrayField(models.TextField('Причины отказа'), blank=True, null=True)
+    refusal_reasons = ArrayField(models.TextField(), verbose_name='Причины отказа', blank=True, null=True)
 
     def __str__(self):
         return self.trademark or self.name
@@ -51,28 +51,28 @@ class Loan(models.Model):
     name = models.TextField('Название займа')
     last_modified = models.DateTimeField('Дата актуализации')
     # ** Условия и ставки
-    purpose = ArrayField(models.TextField('Цель займа'))
+    purpose = ArrayField(models.TextField(), verbose_name='Цель займа')
     amount_max = models.IntegerField('Максимальная сумма займа')
     amount_note = models.TextField('Допинфа по сумме займа', blank=True)  # first_loan_condition
     rate = models.TextField('Ставка')
     period_min = models.IntegerField('Минимальный срок займа')  # dates_from
     period_max = models.IntegerField('Максимальный срок займа')  # dates_to
     period_note = models.TextField('Допинфа по срокам', blank=True)  # loan_time_terms
-    collateral = ArrayField(models.TextField('Обеспечение'))  # providing
+    collateral = ArrayField(models.TextField(), verbose_name='Обеспечение')  # providing
     # ** Требования и документы
-    borrowers_categories = ArrayField(models.TextField('Категория заемщиков'))
+    borrowers_categories = ArrayField(models.TextField(), verbose_name='Категория заемщиков')
     borrowers_age = models.TextField('Возраст заемщика')
-    borrowers_registration = ArrayField(models.TextField('Регистрация'))
-    borrowers_documents = ArrayField(models.TextField('Документы'))
+    borrowers_registration = ArrayField(models.TextField(), verbose_name='Регистрация')
+    borrowers_documents = ArrayField(models.TextField(), verbose_name='Документы')
     # ** Выдача
     issuance = models.TextField('Срок выдачи')
-    loan_processing = ArrayField(models.TextField('Оформление займа'))
-    loan_form = ArrayField(models.TextField('Форма выдачи'))
+    loan_processing = ArrayField(models.TextField(), verbose_name='Оформление займа')
+    loan_form = ArrayField(models.TextField(), verbose_name='Форма выдачи')
     loan_form_note = models.TextField('Допинфа по форме выдачи', blank=True)  # loan_form_description
     # ** Погашение
-    repayment_order = ArrayField(models.TextField('Порядок погашения'))
+    repayment_order = ArrayField(models.TextField(), verbose_name='Порядок погашения')
     repayment_order_note = models.TextField('Допинфа по порядку погашения', blank=True)  # repayment_order_description
-    payment_methods = ArrayField(models.TextField('Способ оплаты'))
+    payment_methods = ArrayField(models.TextField(), verbose_name='Способ оплаты')
 
     # * Об организации
     # We merge based on scraped data, which can be outdated so we keep it for future debugging
