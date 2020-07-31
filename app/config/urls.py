e61eca098c62fad9422c23362aa38fa6b8c2d187
Valerii Ryndin <ryndin20@yandex.ru>
2020-07-31 13:15:58 +0300

    mfo urlpattern change

--- app/config/urls.py
+++ app/config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('lenders/', include('lenders.urls')),
+    path('mfo/', include('lenders.urls')),
     path('admin/', admin.site.urls),
 ]
