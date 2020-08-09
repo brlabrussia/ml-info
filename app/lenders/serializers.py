e358089e84b24879eca42a416a1c79d4fa5364d1
Valerii Ryndin <ryndin20@yandex.ru>
2020-08-09 21:08:56 +0300

    more cbr serializer refactoring

--- app/lenders/serializers.py
+++ app/lenders/serializers.py
@@ -19,6 +19,19 @@ class LoanSerializer(serializers.ModelSerializer):
 
 
 class CbrSerializer(serializers.ModelSerializer):
+    """
+    name: str
+    full_name: str
+    type: str
+    regdate: str
+    regnum: int
+    ogrn: int
+    inn: int
+    website: str
+    email: str
+    address: str
+    """
+
     def to_internal_value(self, data):
         return super().to_internal_value({
             'name': data.get('name', ''),
@@ -66,6 +79,8 @@ class CbrSerializer(serializers.ModelSerializer):
         since serializer is initiated with `many=True`
         so we implement create/update logic here.
         """
+
+        # try to find instance in db
         ogrn = validated_data.get('ogrn')
         inn = validated_data.get('inn')
         regnum = validated_data.get('regnum')
@@ -74,15 +89,17 @@ class CbrSerializer(serializers.ModelSerializer):
             | Q(inn__isnull=False, inn=inn)
             | Q(regnum__isnull=False, regnum=regnum),
         )
+
+        # update if found
         if qs.exists():
             return self.update(qs.first(), validated_data)
 
         # create new if none found
-        lender = Lender(**validated_data)
-        lender.scraped_from = ['https://www.cbr.ru/vfs/finmarkets/files/supervision/list_MFO.xlsx']
-        lender.is_legal = True
-        lender.save()  # Lender.objects.create(**validated_data)
-        return lender
+        return Lender.objects.create(
+            **validated_data,
+            scraped_from=['https://www.cbr.ru/vfs/finmarkets/files/supervision/list_MFO.xlsx'],
+            is_legal=True,
+        )
 
     def update(self, instance, validated_data):
         """
@@ -91,6 +108,7 @@ class CbrSerializer(serializers.ModelSerializer):
         we don't call `save()` for nothing and `updated_at`
         field is meaningful.
         """
+
         fields_to_override = (
             'name',
             'full_name',
@@ -143,3 +161,70 @@ class CbrSerializer(serializers.ModelSerializer):
             'inn': {'validators': [integer_validator]},
             'regnum': {'validators': [integer_validator]},
         }
+
+
+# class ZaymovSerializer(serializers.ModelSerializer):
+#     """
+#     url: str
+#     name: str
+#     logo: str
+#     reg_number: int
+#     ogrn: int
+#     registry_date: str
+#     address: str
+#     """
+
+#     def to_internal_value(self, data):
+#         return super().to_internal_value({
+#             'scraped_from': data.get('url', ''),
+#             'trademark': data.get('name', ''),
+#             'logo': data.get('logo', ''),
+#             'regdate': data.get('registry_date', ''),
+#             'regnum': data.get('reg_number'),
+#             'ogrn': data.get('ogrn'),
+#             'address': data.get('address', ''),
+#         })
+
+#     def create(self, validated_data):
+#         """
+#         Can't override `save()` for this class
+#         since serializer is initiated with `many=True`
+#         so we implement create/update logic here.
+#         """
+#         ogrn = validated_data.get('ogrn')
+#         inn = validated_data.get('inn')
+#         regnum = validated_data.get('regnum')
+#         qs = Lender.objects.filter(
+#             Q(ogrn__isnull=False, ogrn=ogrn)
+#             | Q(inn__isnull=False, inn=inn)
+#             | Q(regnum__isnull=False, regnum=regnum),
+#         )
+#         if qs.exists():
+#             return self.update(qs.first(), validated_data)
+
+#         # create new if none found
+#         lender = Lender(**validated_data)
+#         lender.is_legal = False
+#         lender.save()
+#         return lender
+
+#     def update(self, instance, validated_data):
+#         return instance
+
+#     class Meta:
+#         model = Lender
+#         fields = (
+#             'scraped_from',
+#             'trademark',
+#             'logo',
+#             'regdate',
+#             'regnum',
+#             'ogrn',
+#             'address',
+#         )
+#         extra_kwargs = {
+#             # following fields have `unique=True` constraint
+#             # disable `UniqueValidator` since we manage it in `create()`
+#             'ogrn': {'validators': [integer_validator]},
+#             'regnum': {'validators': [integer_validator]},
+#         }
