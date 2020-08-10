bcd18100841c15ce43263abf5a195fa27ae1f1af
Valerii Ryndin <ryndin20@yandex.ru>
2020-08-10 11:19:39 +0300

    drf filters

--- app/config/settings.py
+++ app/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.SearchFilter',
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 
--- app/config/urls.py
+++ app/config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('mfo/', include('lenders.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
 ]
