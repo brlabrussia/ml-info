eb91d57a5e0deeab6fea1a1108d999a0aaf08c3a
Valerii Ryndin <ryndin20@yandex.ru>
2020-08-10 11:18:44 +0300

    imitate_scrapers util

---
+++ app/lenders/utils/imitate_scrapers.py
@@ -0,0 +1,20 @@
+import json
+from pprint import pprint
+
+import requests
+
+scrapers = (
+    'cbr',
+    'zaymov',
+    'vsezaimyonline',
+    'banki',
+)
+
+for scraper in scrapers:
+    path = f'app/lenders/utils/imitate_scrapers_input/{scraper}.json'
+    with open(path) as f:
+        r = requests.post(
+            f'http://localhost:8080/mfo/scrapers/{scraper}/',
+            json=json.load(f),
+        )
+        pprint(r.json())
