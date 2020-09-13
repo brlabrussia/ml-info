968a354b62f2c316521a06146489176830ddd43f
Valerii Ryndin <ryndin20@yandex.ru>
2020-09-14 02:51:47 +0300

    prod setup

---
+++ app/config/celery.py
@@ -0,0 +1,18 @@
+# https://docs.celeryproject.org/en/stable/django/first-steps-with-django.html
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('info')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
