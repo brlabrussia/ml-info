04a1e2570f2097ee57e9e0a02bbde38c4d5e0678
Valerii Ryndin <ryndin20@yandex.ru>
2020-09-21 06:34:57 +0300

    tables - basic prototype

--- app/tables/models.py
+++ app/tables/models.py
@@ -1,3 +1,24 @@
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    name = models.TextField()
+    description = models.TextField(blank=True)
+    scraper = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Table(models.Model):
+    category = models.TextField()
+    name = models.TextField()
+    url = models.URLField()
+    description = models.TextField(blank=True)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='tables', blank=True, null=True)
+    driver_args = ArrayField(models.TextField(), blank=True, null=True)
+    result = JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
