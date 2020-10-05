bddab8171d1db7a934c655bc91d7f7b910509cb4
Valerii Ryndin <51380972+ryndin20@users.noreply.github.com>
2020-10-05 09:34:27 +0300

    interoperability with scrapy (#6)
    * refactor recent fixes
    
    * mfo initial migration
    
    * init app
    
    * celery setup and project rename
    
    * tables - basic prototype
    
    * readme
    
    * Update README.md
    
    * tables - interoperability with scrapy

--- app/tables/models.py
+++ app/tables/models.py
@@ -22,3 +22,6 @@ class Table(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['-category']
---
+++ app/tables/tasks.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+import requests
+
+from core.celery import app
+from tables.models import Table
+
+SCRAPYD_URL = os.getenv('SCRAPYD_URL')
+SCRAPYD_LOGIN = os.getenv('SCRAPYD_LOGIN')
+SCRAPYD_PASS = os.getenv('SCRAPYD_PASS')
+
+
+@app.task(ignore_result=True)
+def schedule_scrapers():
+    project = 'tables'
+    webhook = 'http://eb5db0e8077d.ngrok.io/tables/scrappers/'  # f"https://{os.getenv('VIRTUAL_HOST')}/tables/scrapers/"
+    tables = Table.objects.filter(driver__isnull=False)
+    for table in tables:
+        spider = table.driver.scraper
+        args = [table.pk, table.url, *table.driver_args]
+        data = [
+            ('project', project),
+            ('spider', spider),
+            ('request_args', json.dumps(args)),
+            ('setting', f'WEBHOOK_ENDPOINT={webhook}'),
+        ]
+        requests.post(
+            f'{SCRAPYD_URL}/schedule.json',
+            data=data,
+            auth=(SCRAPYD_LOGIN, SCRAPYD_PASS),
+        )
