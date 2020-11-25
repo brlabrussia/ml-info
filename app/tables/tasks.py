72bdffc3cdb9249588abb594f4e811ce8bd9372b
Valerii Ryndin <ryndin20@yandex.ru>
2020-11-25 22:48:28 +0300

    tables.tasks refactor

--- app/tables/tasks.py
+++ app/tables/tasks.py
@@ -1,53 +1,49 @@
 import json
 import os
+from typing import List
 
 import requests
 
 from core.celery import app
 from tables.models import Table
 
+SCRAPYD_PROJECT = 'tables'
 SCRAPYD_URL = os.getenv('SCRAPYD_URL')
 SCRAPYD_LOGIN = os.getenv('SCRAPYD_LOGIN')
 SCRAPYD_PASS = os.getenv('SCRAPYD_PASS')
 
+VIRTUAL_HOST = (
+    VIRTUAL_HOST
+    if 'localhost' not in (VIRTUAL_HOST := os.getenv('VIRTUAL_HOST'))
+    else 'nginx'
+)
+WEBHOOK_ENDPOINT = f'http://{VIRTUAL_HOST}/tables/scrapers/'
+
+
+@app.task(ignore_result=True)
+def schedule_scraper(spider: str, pk: int, url: str, driver_args: List[str]):
+    spider = spider
+    args = [pk, url, *driver_args]
+    data = [
+        ('project', SCRAPYD_PROJECT),
+        ('spider', spider),
+        ('request_args', json.dumps(args)),
+        ('setting', f'WEBHOOK_ENDPOINT={WEBHOOK_ENDPOINT}'),
+    ]
+    requests.post(
+        f'{SCRAPYD_URL}/schedule.json',
+        data=data,
+        auth=(SCRAPYD_LOGIN, SCRAPYD_PASS),
+    )
+
 
 @app.task(ignore_result=True)
 def schedule_scrapers():
-    project = 'tables'
-    webhook = f'https://{os.getenv("VIRTUAL_HOST")}/tables/scrapers/'
     tables = Table.objects.filter(driver__isnull=False)
     for table in tables:
-        spider = table.driver.scraper
-        args = [table.pk, table.url, *table.driver_args]
-        data = [
-            ('project', project),
-            ('spider', spider),
-            ('request_args', json.dumps(args)),
-            ('setting', f'WEBHOOK_ENDPOINT={webhook}'),
-        ]
-        requests.post(
-            f'{SCRAPYD_URL}/schedule.json',
-            data=data,
-            auth=(SCRAPYD_LOGIN, SCRAPYD_PASS),
-        )
-
-
-@app.task(ignore_result=True)
-def schedule_hltv_scrapers():
-    project = 'tables'
-    webhook = f'https://{os.getenv("VIRTUAL_HOST")}/tables/scrapers/'
-    tables = Table.objects.filter(driver__isnull=False, url__icontains='hltv.org')
-    for table in tables:
-        spider = table.driver.scraper
-        args = [table.pk, table.url, *table.driver_args]
-        data = [
-            ('project', project),
-            ('spider', spider),
-            ('request_args', json.dumps(args)),
-            ('setting', f'WEBHOOK_ENDPOINT={webhook}'),
-        ]
-        requests.post(
-            f'{SCRAPYD_URL}/schedule.json',
-            data=data,
-            auth=(SCRAPYD_LOGIN, SCRAPYD_PASS),
+        schedule_scraper.delay(
+            table.driver.scraper,
+            table.pk,
+            table.url,
+            table.driver_args,
         )
