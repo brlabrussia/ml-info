c214a54ead5d416a323b0deba3e5552523f9a4cd
Valerii Ryndin <ryndin20@yandex.ru>
2020-11-27 04:50:11 +0300

    add preview to admin list

--- app/tables/admin.py
+++ app/tables/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Driver, Table
 from .tasks import schedule_scraper
@@ -7,16 +9,24 @@ from .tasks import schedule_scraper
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     readonly_fields = [
-        'result',
         'updated_at',
         'created_at',
     ]
 
+    exclude = [
+        'result',
+    ]
+
     list_display = [
+        'preview',
         'name',
         'url',
         'category',
         'driver',
+    ]
+
+    list_display_links = [
+        'name',
     ]
 
     search_fields = [
@@ -33,6 +43,12 @@ class TableAdmin(admin.ModelAdmin):
         'schedule',
     ]
 
+    @mark_safe
+    def preview(self, obj):
+        return '<a class="button" href="{}">Preview</a>'.format(
+            reverse('preview', args=[obj.pk]),
+        )
+
     def schedule(self, request, queryset):
         for obj in queryset:
             schedule_scraper.delay(
@@ -42,6 +58,7 @@ class TableAdmin(admin.ModelAdmin):
                 obj.driver_args,
             )
     schedule.short_description = 'Schedule selected tables'
+    schedule.allowed_permissions = ['delete']
 
 
 @admin.register(Driver)
