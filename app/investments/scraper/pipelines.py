41401528b183740827dfba443f7954cf344f1d6a
Valerii Ryndin <ryndin20@yandex.ru>
2020-12-18 13:18:11 +0300

    spider-specific djangowriterpipeline

--- app/investments/scraper/pipelines.py
+++ app/investments/scraper/pipelines.py
@@ -1,5 +1,17 @@
 class DjangoWriterPipeline:
     def process_item(self, item, spider):
+        custom_method = getattr(self, spider.name, None)
+        if custom_method is None:
+            spider.logger.error(f'Cannot find custom process_item method for {spider.name}')
+            return item
+        return custom_method(item, spider)
+
+    def banki_shares(self, item, spider):
         if item.is_valid():
-            item.save()
+            item.django_model.objects.update_or_create(
+                isin=item['isin'],
+                defaults=item,
+            )
+        else:
+            spider.logger.error(f'Invalid item {item}')
         return item
