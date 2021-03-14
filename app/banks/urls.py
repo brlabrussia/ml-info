f37c83d5cb06ae6f0b39d20be8638e888e1da672
Valerii Ryndin <ryndin20@yandex.ru>
2021-03-15 02:54:09 +0300

    banks cleanup

--- app/banks/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from banks import views
-
-urlpatterns = []
-
-router = SimpleRouter()
-router.register(r'rating', views.RatingViewSet)
-urlpatterns += router.urls
