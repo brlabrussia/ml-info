e1de49c090d2237087069939c681309cf8d6fd40
Valerii Ryndin <ryndin20@yandex.ru>
2021-03-22 21:35:38 +0300

    admin redirect

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -7,13 +8,14 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('tables/', include('tables.urls')),
-   path('other/', include('other.urls')),
+    path('', RedirectView.as_view(url='admin/')),
+    path('admin/', admin.site.urls),
+    path('tables/', include('tables.urls')),
+    path('other/', include('other.urls')),
 
-   re_path(r'^api-auth/', include('rest_framework.urls')),
-   path('api/v1/', include('api.v1.urls')),
-   path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-   path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-   path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('api.v1.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
